78c790dd4fecf8a8df7b857d9896ce214b60bdc9
Pol528 <68276676+Pol528@users.noreply.github.com>
2021-01-07 10:20:15 +0200

    softban

---
+++ commands/softban.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'softban',
+    description: 'softban someone',
+    async execute(client, message, args) {
+        if(!message.member.hasPermission('BAN_MEMBERS'))
+        return message.reply("you can't use that here!");
+        if (!message.guild.me.permissions.has("BAN_MEMBERS")) {
+            return message.reply('I do not have permissions, please contact an administrator');
+        }
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if(!member)
+          return message.reply("You need to tell me who to softban!");
+        if(!member.kickable) 
+          return message.reply("i can't softban that member!");
+        if(member.id === message.author.id) return message.reply(`You can't softban yourself!`)
+        //ban
+        await member.ban({ days: 7, reason: 'softbanned' })
+      .catch(error => message.reply(`${message.author}, there was an error: ${error}`));
+      //unban
+      message.guild.members.unban(member.id)
+
+      const success = new MessageEmbed()
+      .setTitle('Success!')
+      .setDescription(`Member softbanned!`)
+      .addField(`member:`, `${member.user.tag}`)
+      //.addField(`with reason:`, `none`)
+      .addField(`softbanned by:`, `${message.author.tag}`)
+      .setColor(`GREEN`)
+      .setTimestamp()
+    message.channel.send(success);
+    }}

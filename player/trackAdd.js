bdd3f7052f5f3d85f0ad132998f72732de66aec4
Pol528 <68276676+Pol528@users.noreply.github.com>
2021-01-07 11:05:48 +0200

    play command revamp

--- player/trackAdd.js
+++ player/trackAdd.js
@@ -3,7 +3,8 @@ const { MessageEmbed } = require("discord.js");
 module.exports = (client, message, queue, track) => {
     const sucesss = new MessageEmbed()
             .setTitle('Playlist')
-            .setDescription(`${client.emotes.music} - ${track.title} has been added to the queue!`)
+            .setDescription(`${client.emotes.music} - [${track.title}](${track.url}) has been added to the queue!`)
+            .setThumbnail(`${track.thumbnail}`)
             .setColor(`GREEN`)
             .setTimestamp()
     message.channel.send(sucesss);
--- player/trackStart.js
+++ player/trackStart.js
@@ -2,7 +2,8 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = (client, message, track) => {
     const start = new MessageEmbed()
-        .setDescription(`${client.emotes.music} - Now playing [${track.title}](${track.url}) into ${message.member.voice.channel.name}!`)
+        .setDescription(`${client.emotes.music} - Now playing [${track.title}](${track.url})!`)
+        .addField(`Playing into`, `${message.member.voice.channel.name}`)
         .setThumbnail(track.thumbnail)
         .setTimestamp()
         .setColor(`GREEN`)
